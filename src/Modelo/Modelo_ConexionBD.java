8090258203f033bd048840b81cff0977dfa64e6d
Gaby_Vera_P <gabriela.vera.est@tecazuay.edu.ec>
2021-09-20 22:31:14 -0500

    Conexion a la base de datos postgresql

---
+++ src/Modelo/Modelo_ConexionBD.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Pandora
+ */
+public class Modelo_ConexionBD {
+    String cadenaConexion="jdbc:postgresql://localhost:5432/Catering";
+    String usuario="postgres";
+    String contraseña="1234";
+    
+    Connection con;
+
+    public void establecer_conexion() {
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Modelo_ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            con= DriverManager.getConnection(cadenaConexion, usuario, contraseña);
+            System.out.println("conexion exitosa");
+        } catch (Exception ex) {
+            Logger.getLogger(Modelo_ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ResultSet Consulta(String sqlc){//para la conexion
+        try {
+            Statement st=con.createStatement();
+            ResultSet rb=st.executeQuery(sqlc);
+            return rb;
+        } catch (SQLException ex) {
+            Logger.getLogger(Modelo_ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    
+    }    
+}
